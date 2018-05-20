38b68c4e088ee9a6b19eb889b0c4e2b6b7cdb2ba
sriks123 <srksriks123@gmail.com>
2018-05-20 23:25:47 +0530

    Added views to filter stops, route_master and route relations and store them to show on map

--- osm/urls.py
+++ osm/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    url(r'^$',views.load,name="map")
+    url(r'^$',views.home,name="home"),
+    url(r'^stops/',views.get_stops, name="stops"),
+    url(r'^osm/', views.load, name="load"),
 ]
--- osmdatamodel/urls.py
+++ osmdatamodel/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.urls import path
-
+from osm import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',include('osm.urls'))
+    url(r'^$',include('osm.urls')),
+    url(r'^stops/',views.get_stops, name="stops"),
+    url(r'^osm/', views.load, name="load"),
+    url(r'^route_masters', views.get_route_master_relations, name="route_master"),
 ]
